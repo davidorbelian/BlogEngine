741329211b130dec80b613d3371de12871540eb8
David Orbelian <david.orbelian@gmail.com>
2020-04-12 20:58:45 +0400

    Create DeleteCommentCommand and DeleteCommentCommandHandler.

---
+++ src/BlogEngine.Application/Comments/DeleteCommentCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BlogEngine.Application.Exceptions;
+using MediatR;
+
+namespace BlogEngine.Application.Comments
+{
+    // ReSharper disable once UnusedMember.Global
+    public sealed class DeleteCommentCommandHandler : IRequestHandler<DeleteCommentCommand>
+    {
+        private readonly IBlogEngineContext _context;
+
+        public DeleteCommentCommandHandler(IBlogEngineContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(
+            DeleteCommentCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var comment = await _context.Comments.FindAsync(request.Id) ??
+                          throw new CommentNotFoundException(request.Id);
+
+            if (comment.ArticleId != request.ArticleId)
+                throw new CommentNotFoundException(request.Id);
+
+            _context.Comments.Remove(comment);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
