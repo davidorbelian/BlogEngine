12b04ae4e6e75e492c95ecfecb901bfae664c32d
David Orbelian <david.orbelian@gmail.com>
2020-04-12 20:11:06 +0400

    Create UpdateArticleCommand and UpdateArticleCommandHandler.

---
+++ src/BlogEngine.Application/Articles/UpdateArticleCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace BlogEngine.Application.Articles
+{
+    public sealed class UpdateArticleCommand : IRequest<string>
+    {
+        public UpdateArticleCommand(string id, string title, string content)
+        {
+            Id = id;
+            Title = title;
+            Content = content;
+        }
+
+        public string Id { get; }
+        public string Title { get; }
+        public string Content { get; }
+    }
+}
